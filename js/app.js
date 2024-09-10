865602d8ef5b20dc4d27bdb65a811f2c7b8ba603
יהודית טמינו <taminojudit@hotmail.co.il>
2024-09-10 13:21:30 +0300

    add product data

--- js/app.js
+++ js/app.js
@@ -1,45 +1,44 @@
+const inputProductName = document.getElementById("product-input--name");
+const inputProductQuantity = document.getElementById("product-input--qty"),
+  inputProductQuantityName = document.getElementById("product-input--qty-name");
+const inputProductPrice = document.getElementById("product-input--price");
 const productsList = document.querySelector(".list-products");
 
 const addProduct = () => {
-
-
   // create tr element
   const tr = document.createElement("tr");
   tr.className = "product";
 
-
-
-
-
-
-
-  tdData.appendChild(divDescription);
-
-  // create span element thah display the price of the product
-  // const spanPrice = document.createElement("span");
-  // spanPrice.className = "price";
-  // spanPrice.innerText = `$${priceInput}`;
-  // tdData.appendChild(spanPrice);
-
-
-  tr.appendChild(tdData);
+  addProdactData(tr);
   productsList.appendChild(tr);
 }
 
 addProdactData = (tr) => {
-  const productInput = document.getElementById("product-input--name").value;
-  const quantityInput = parseInt(document.getElementById("product-input--qty").value);
-  const priceInput = parseFloat(document.getElementById("product-input--price").value);
+  const productTd = document.createElement("td");
 
-  // create dt element
-  const tdData = document.createElement("td");
-  tdData.className = "product-data";
+  // div element that display the name and the amount of the product
+  const productDescription = document.createElement("div");
+  productDescription.className = "product-description";
 
-  const divDescription = document.createElement("div");
-  divDescription.className = "product-description";
-  // creatin 2 span element that display the amount and name of the product
-  divDescription.innerHTML = `
-  <span class="qty">${quantityInput}</span>
-  <span class="name">${productInput}</span>
-  `;
+  // add product name to the list
+  const productName = document.createElement("span");
+  productName.className = "product-name";
+  productName.innerText = inputProductName.value;
+  productDescription.appendChild(productName);
+
+
+  // add product quantity to the list
+  const productQuantity = document.createElement("span");
+  productQuantity.classList = "product-qty";
+  productQuantity.innerText = ` | ${inputProductQuantity.value}${inputProductQuantityName.value}`;
+  productDescription.appendChild(productQuantity);
+
+  // add product price to the list
+  const productPrice = document.createElement("p");
+  productPrice.className = "product-price";
+  productPrice.innerText = `$${inputProductPrice.value}`;
+
+  productTd.appendChild(productDescription);
+  productTd.appendChild(productPrice);
+  tr.appendChild(productTd);
 }
