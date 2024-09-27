b2aea01b85fa957b42131e796f1c1541e88c0241
יהודית טמינו <taminojudit@hotmail.co.il>
2024-09-27 11:38:03 +0300

    project done

--- js/app.js
+++ js/app.js
@@ -1,75 +1,61 @@
-/*=============== SUMMARY ===============*/
+/*=============== VARIABLES ===============*/
+// SUMMARY
 const totalQty = document.getElementById("total-qty");
 const totalPrice = document.getElementById("total-price");
 let counterPrice = 0, counterQty = 0;
 let cost = 0, qty = 0;
-
-/*=============== DISPLAY ITEMS ===============*/
+// DISPLAY ITEMS
 const displayItems = document.querySelector(".display-items");
-
-/*=============== ADD ITEMS ===============*/
-// VARIABLES
+// ADD ITEMS
 const itemName = document.getElementById("name");
 const qtyNumber = document.getElementById("quantity-number");
-const qtyTxt = document.getElementById("quantity-txt");
 const price = document.getElementById("price");
 
-// function that add item to the list
-function addItem() {
-  const li = document.createElement("li");
 
-  // display the name, quantity and the price of the item
-  const itemDesription = document.createElement("div");
-  itemDesription.innerHTML = `
+// func => add item to the list
+function addItem() {
+  if (itemName.value != "" && qtyNumber.value != "" && price.value != "") {
+    const li = document.createElement("li");
+
+    // display the name, quantity and the price of the item
+    const itemDesription = document.createElement("div");
+    itemDesription.innerHTML = `
     <span class="item-name">${itemName.value}</span> | 
-    <span class="item-qty">${qtyNumber.value}${qtyTxt.value}</span>
+    <span class="item-qty">${qtyNumber.value}</span>
     <p class="item-price">$${price.value}</p>`;
 
-  // display delete button
-  const itemEdit = document.createElement("div");
-  itemEdit.innerHTML = `
+    // display delete button
+    const itemEdit = document.createElement("div");
+    itemEdit.innerHTML = `
     <a href="#" class="btn-delete">
       <i class="ri-delete-bin-line"></i>
-    </a>
-  `;
+    </a>`;
 
-  // add class name to the new element for style
-  itemDesription.className = "item-description";
-  itemEdit.className = "item-edit";
+    // add class name to the new element for style
+    itemDesription.className = "item-description";
+    itemEdit.className = "item-edit";
 
-  li.appendChild(itemDesription);
-  li.appendChild(itemEdit);
-  displayItems.appendChild(li);
+    li.appendChild(itemDesription);
+    li.appendChild(itemEdit);
+    displayItems.appendChild(li);
 
-  checkQty();
-  clearInput();
+    counterPrice = price.value;
+    counterQty = qtyNumber.value;
+
+    calculateTotalPriceAndQty("+");
+    clearInput();
+    saveList();
+  }
 }
 
-// function that clear the input fileds after item been added.
+// func => clear the input fileds after item been added.
 function clearInput() {
   itemName.value = "";
   qtyNumber.value = "";
-  qtyTxt.value = "g";
   price.value = "";
 }
 
-
-// function that check the qty
-// if qty is g or kg => assigns counterQty = 1;
-function checkQty() {
-  switch (qtyTxt.value) {
-    case "g":
-    case "kg":
-      counterQty = 1;
-      break;
-    default:
-      counterQty = qtyNumber.value;
-  }
-  counterPrice = price.value;
-  calculateTotalPriceAndQty("+");
-}
-
-// function that calculate the total price and quantity and display it.
+// func => calculate the total price and quantity and display it.
 function calculateTotalPriceAndQty(operator) {
   counterPrice = parseFloat(counterPrice);
   counterQty = parseInt(counterQty);
@@ -93,21 +79,23 @@ displayItems.addEventListener('click', (e) => {
     e.target.classList.toggle("checked");
 
     // get price and quantity values of select item
-    const itemSelected = e.target;
-    getPriceAndQty(itemSelected);
+    getPriceAndQty(e.target);
 
     /* if item checked => subtract the item price & quantity from summary */
     if (e.target.classList.contains("checked"))
       calculateTotalPriceAndQty("-");
     else
       calculateTotalPriceAndQty("+");
-  } else if (e.target.tagName === "I") 
+  } else if (e.target.tagName === "I")
     removeItem(e);
+
+  saveList();
 });
 
+// func => remove item from list
 function removeItem(e) {
   const li = e.target.parentElement.closest("li");
-  
+
   if (!li.classList.contains("checked") && cost > 0 && qty > 0) {
     getPriceAndQty(li);
     calculateTotalPriceAndQty("-");
@@ -115,6 +103,7 @@ function removeItem(e) {
   li.remove();
 }
 
+// func => get price and quantity from selected item
 function getPriceAndQty(itemSelected) {
   const filteredItems = Array.from(itemSelected.childNodes[0].children).filter(item => {
     return item.classList.contains("item-qty") || item.classList.contains("item-price");
@@ -122,9 +111,39 @@ function getPriceAndQty(itemSelected) {
 
   for (let i = 0; i < filteredItems.length; i++) {
     if (filteredItems[i].classList.contains("item-qty"))
-      counterQty = filteredItems[i].innerText.replace(/[^0-9\.]+/g, "");
+      counterQty = filteredItems[i].innerText;
     else
       counterPrice = filteredItems[i].innerText.replace("$", "");
+  }
+}
+
+// func => auto save the list
+function saveList() {
+  if (typeof (Storage) !== "undefined") {
+    localStorage.setItem("list", displayItems.innerHTML);
+    localStorage.setItem("totalPrice", totalPrice.innerText);
+    localStorage.setItem("totalQty", totalQty.innerText);
+  }
+}
+
+// display the saved list from local storage
+window.onload = () => {
+  let storagePrice = localStorage.getItem("totalPrice"),
+    storageQty = localStorage.getItem("totalQty");
+
+  if (typeof (Storage) !== "undefined") {
+    displayItems.innerHTML = localStorage.getItem("list");
+
+    if (storagePrice != null || storageQty != null) {
+      totalPrice.innerText = storagePrice;
+      cost = parseFloat(storagePrice);
+
+      totalQty.innerText = storageQty;
+      qty = parseInt(storageQty);
+    } else {
+      totalPrice.innerText = "$0.00";
+      totalQty.innerText = "0";
+    }
   }
 }
 
